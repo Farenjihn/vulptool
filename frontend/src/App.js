6b7c3e2a742d85aca2dff045a7f942109e345fb2
Lawrence <bykow12345@gmail.com>
2018-06-06 17:34:51 +0200

    Week picker

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,15 @@ import Welcome from './Welcome';
 
 
 
-import {
-    BrowserRouter as Router,
-    Route,
-    Link
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Layout, Menu, Breadcrumb, Icon } from 'antd';
 import { List, Row, Col } from 'antd';
 import { Card, Avatar } from 'antd';
+
 const { Meta } = Card;
 const { Header, Content, Footer, Sider } = Layout;
 const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 const IconText = ({ type, text }) => (
     <span>
     <Icon type={type} style={{ marginRight: 8 }} />
@@ -24,30 +22,42 @@ const IconText = ({ type, text }) => (
 );
 
 
-
 class SiderDemo extends React.Component {
     state = {
         collapsed: false,
+        current: 'welcome',
     };
     onCollapse = (collapsed) => {
         console.log(collapsed);
         this.setState({ collapsed });
+    }
+    handleMenuClick = (e) => {
+        console.log('click ', e);
+        this.setState({
+            current: e.key,
+        });
     }
     render() {
         return (
             <Router>
                 <div>
                     <Header className="header">
-                        <div className="logo" />
-                        <Menu
-                            theme="dark"
-                            mode="horizontal"
-                            defaultSelectedKeys={['3']}
-                            style={{lineHeight: '64px' }}
-                        >
-                            <Menu.Item key="1">nav 1</Menu.Item>
-                            <Menu.Item key="2">nav 2</Menu.Item>
-                            <Menu.Item key="3">nav 3</Menu.Item>
+                        <Menu theme="dark" mode="horizontal">
+                            <Menu.Item key="login">
+                                Login
+                            </Menu.Item>
+                            {/*<SubMenu title={<span><Icon type="setting" />Account</span>}>
+                                <MenuItemGroup title="Item 1">
+                                    <Menu.Item key="setting:1">Settings</Menu.Item>
+                                    <Menu.Item key="setting:2">Characters</Menu.Item>
+                                </MenuItemGroup>
+                                <MenuItemGroup title="Item 2">
+                                    <Menu.Item key="setting:3">Logout</Menu.Item>
+                                </MenuItemGroup>
+                            </SubMenu>*/}
+                            <Menu.Item key="feedback">
+                                <a href="https://google.com" target="_blank" rel="noopener noreferrer">{<span><Icon type="mail"/>Feedback</span>}</a>
+                            </Menu.Item>
                         </Menu>
                     </Header>
                     <Layout style={{ minHeight: '100vh' }}>
@@ -56,18 +66,21 @@ class SiderDemo extends React.Component {
                             collapsed={this.state.collapsed}
                             onCollapse={this.onCollapse}
                         >
-                            <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
-                                <Menu.Item key="1">
+                            <Menu theme="dark" mode="inline"
+                                  onClick={this.handleMenuClick}
+                                  selectedKeys={[this.state.current]}
+                            >
+                                <Menu.Item key="welcome">
                                     <Link to={"/welcome"} />
                                     <Icon type="home" />
                                     <span>Home</span>
                                 </Menu.Item>
-                                <Menu.Item key="2">
+                                <Menu.Item key="calendar">
                                     <Link to={"/calendar"} />
                                     <Icon type="calendar" />
                                     <span>Calendar</span>
                                 </Menu.Item>
-                                <Menu.Item key="3">
+                                <Menu.Item key="roster">
                                     <Link to={"/roster"} />
                                     <Icon type="team" />
                                     <span>Roster</span>
